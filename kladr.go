f5deb3f470fc005e5615dc8056ad3b05f207cc09
jonywtf <jonywtf37@gmail.com>
2015-07-22 08:14:49 +0300

    fix: Import() return Kladr{}

--- kladr.go
+++ kladr.go
@@ -2,8 +2,6 @@ package fias
 
 import (
 	"errors"
-	"unsafe"
-	"log"
 )
 
 type Kladr struct {
@@ -15,41 +13,33 @@ type Kladr struct {
 	AltNames []AltNamesRecod
 }
 
-const base_path = "C:\\temp\\kladr\\base\\"
-
-func Import() error {
-
-	log.Println("sizeof DbfHead", unsafe.Sizeof(DbfHead{}))
-	log.Println("sizeof DBaseField", unsafe.Sizeof(DBaseField{}))
-	log.Println("sizeof DBase7Field", unsafe.Sizeof(DBase7Field{}))
-	log.Println("")
+func Import(base_path string) (Kladr, error) {
 
 	var kladr = Kladr{}
 	var err error
 	kladr.AltNames, err = ImportAltNames(base_path + "ALTNAMES.DBF")
 	if err != nil {
-		return errors.New("Не могу импортировать КЛАДР ALTNAMES.DBF:" + err.Error())
+		return kladr, errors.New("Не могу импортировать КЛАДР ALTNAMES.DBF:" + err.Error())
 	}
 	kladr.SockBase, err = ImportSockBase(base_path + "SOCRBASE.DBF")
 	if err != nil {
-		return errors.New("Не могу импортировать КЛАДР SOCRBASE.DBF:" + err.Error())
+		return kladr, errors.New("Не могу импортировать КЛАДР SOCRBASE.DBF:" + err.Error())
 	}
 	kladr.Flat, err = ImportFlat(base_path + "FLAT.DBF")
 	if err != nil {
-		return errors.New("Не могу импортировать КЛАДР FLAT.DBF:" + err.Error())
+		return kladr, errors.New("Не могу импортировать КЛАДР FLAT.DBF:" + err.Error())
 	}
 	kladr.Kladr, err = ImportKladr(base_path + "KLADR.DBF")
 	if err != nil {
-		return errors.New("Не могу импортировать КЛАДР KLADR.DBF:" + err.Error())
+		return kladr, errors.New("Не могу импортировать КЛАДР KLADR.DBF:" + err.Error())
 	}
 	kladr.Doma, err = ImportDoma(base_path + "DOMA.DBF")
 	if err != nil {
-		return errors.New("Не могу импортировать КЛАДР DOMA.DBF:" + err.Error())
+		return kladr, errors.New("Не могу импортировать КЛАДР DOMA.DBF:" + err.Error())
 	}
 	kladr.Streets, err = ImportStreet(base_path + "STREET.DBF")
 	if err != nil {
-		return errors.New("Не могу импортировать КЛАДР STREET.DBF:" + err.Error())
+		return kladr, errors.New("Не могу импортировать КЛАДР STREET.DBF:" + err.Error())
 	}
-	log.Println("Считываение *.DBF файлов успешно завершено")
-	return nil
+	return kladr, nil
 }
